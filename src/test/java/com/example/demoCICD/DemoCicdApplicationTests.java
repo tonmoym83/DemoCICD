f9bf9a14c69a2ff57361ed3eb38db725f8265612
tonmoym83 <contacttonmoy@gmail.com>
2025-11-23 15:26:01 +0000

    Initial commit of Spring Boot project

---
+++ src/test/java/com/example/demoCICD/DemoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
